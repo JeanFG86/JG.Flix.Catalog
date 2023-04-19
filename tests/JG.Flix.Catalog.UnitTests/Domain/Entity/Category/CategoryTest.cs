e4be161042915fe3d14514178a1139798d96d739
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-04-19 20:56:39 -0300

    Add project files.

---
+++ tests/JG.Flix.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace JG.Flix.Catalog.UnitTests.Domain.Entity.Category;
+
+public class CategoryTest
+{
+    [Fact(DisplayName = nameof(Instantiate))]
+    [Trait("Domain", "Category - Aggregates")]
+    public void Instantiate()
+    {
+        //Arange
+        var validData = new
+        {
+            Name = "category name",
+            Description = "category description",
+        };
+
+        //Act
+        var category = new Category(validData.Name, validData.Description);
+
+        //Assert
+        Assert.NotNull(category);
+        Assert.Equal(validData.Name, category.Name);
+        Assert.Equal(validData.Description, category.Description);
+    }
+}
+
