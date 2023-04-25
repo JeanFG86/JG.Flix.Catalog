d28369bba3ebdc5842ff0c3ec86511caad10a280
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-04-24 21:27:42 -0300

    create category with isActive

--- tests/JG.Flix.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
+++ tests/JG.Flix.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -28,5 +28,31 @@ public class CategoryTest
         Assert.True(category.CreatedAt < datetimeAfter);
         Assert.True(category.IsActive);
     }
+
+    [Theory(DisplayName = nameof(InstantiateWithIsActive))]
+    [Trait("Domain", "Category - Aggregates")]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void InstantiateWithIsActive(bool isActive)
+    {
+        var validData = new
+        {
+            Name = "category name",
+            Description = "category description",
+        };
+        var datetimeBefore = DateTime.Now;
+
+        var category = new DomainEntity.Category(validData.Name, validData.Description, isActive);
+        var datetimeAfter = DateTime.Now;
+
+        Assert.NotNull(category);
+        Assert.Equal(validData.Name, category.Name);
+        Assert.Equal(validData.Description, category.Description);
+        Assert.NotEqual(default(Guid), category.Id);
+        Assert.NotEqual(default(DateTime), category.CreatedAt);
+        Assert.True(category.CreatedAt > datetimeBefore);
+        Assert.True(category.CreatedAt < datetimeAfter);
+        Assert.Equal(isActive, category.IsActive);
+    }
 }
 
