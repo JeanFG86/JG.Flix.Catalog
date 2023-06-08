08a3e10ff5fbf8a1d5f379621bba066ae849c6f8
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-06-08 14:56:23 -0300

    create activate method

--- tests/JG.Flix.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
+++ tests/JG.Flix.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -109,5 +109,21 @@ public class CategoryTest
         var exception = Assert.Throws<EntityValidationException>(action);
         Assert.Equal("Description should be less or equal 10_000 characteres long", exception.Message);
     }
+
+    [Fact(DisplayName = nameof(Activate))]
+    [Trait("Domain", "Category - Aggregates")]
+    public void Activate()
+    {
+        var validData = new
+        {
+            Name = "category name",
+            Description = "category description",
+        };
+
+        var category = new DomainEntity.Category(validData.Name, validData.Description, false);
+        category.Activate();
+
+        Assert.True(category.IsActive);
+    }
 }
 
