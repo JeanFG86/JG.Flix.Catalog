4b081b026c74dd81a148796381dcaa5de957d781
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-06-19 16:33:57 -0300

    create DomainValidation and DomainValidationTest

---
+++ src/JG.Flix.Catalog.Domain/Validation/DomainValidation.cs
@@ -0,0 +1,15 @@
+﻿using JG.Flix.Catalog.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JG.Flix.Catalog.Domain.Validation;
+public class DomainValidation
+{
+    public static void NotNull(object target, string fieldName)
+    {
+        if(target == null) throw new EntityValidationException($"{fieldName} should not be null");
+    }
+}
---
+++ tests/JG.Flix.Catalog.UnitTests/Domain/Validation/DomainValidationTest.cs
@@ -0,0 +1,29 @@
+﻿using Bogus;
+using FluentAssertions;
+using JG.Flix.Catalog.Domain.Exceptions;
+using JG.Flix.Catalog.Domain.Validation;
+using Xunit;
+
+namespace JG.Flix.Catalog.UnitTests.Domain.Validation;
+public class DomainValidationTest
+{
+    private Faker Faker { get; set; } = new Faker();
+
+    [Fact(DisplayName = nameof(NotNullOk))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    public void NotNullOk()
+    {
+        var value = Faker.Commerce.ProductName();
+        Action action = () => DomainValidation.NotNull(value, "Value");
+        action.Should().NotThrow();
+    }
+
+    [Fact(DisplayName = nameof(NotNullThrowWhenNull))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    public void NotNullThrowWhenNull()
+    {
+        string value = null;
+        Action action = () => DomainValidation.NotNull(value, "FieldName");
+        action.Should().Throw<EntityValidationException>().WithMessage("FieldName should not be null");
+    }
+}
