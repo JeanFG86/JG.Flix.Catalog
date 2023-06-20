b47409047ccf0ad880c88a2bb0cf5e0177ec8ba3
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-06-20 20:37:44 -0300

    create test MinLength validation

--- tests/JG.Flix.Catalog.UnitTests/Domain/Validation/DomainValidationTest.cs
+++ tests/JG.Flix.Catalog.UnitTests/Domain/Validation/DomainValidationTest.cs
@@ -2,12 +2,28 @@
 using FluentAssertions;
 using JG.Flix.Catalog.Domain.Exceptions;
 using JG.Flix.Catalog.Domain.Validation;
+using JG.Flix.Catalog.UnitTests.Domain.Entity.Category;
 using Xunit;
 
 namespace JG.Flix.Catalog.UnitTests.Domain.Validation;
 public class DomainValidationTest
 {
     private Faker Faker { get; set; } = new Faker();
+
+    public static IEnumerable<object[]> GetValuesLessThanMin(int numberOfTests = 5)
+    {
+        yield return new object[] { "123456", 10 };
+        var faker = new Faker();
+        for (int i = 0; i < numberOfTests; i++)
+        {
+            var exemple = faker.Commerce.ProductName();
+            var minLength = exemple.Length + (new Random().Next(1,20));
+            yield return new object[]
+            {
+                exemple, minLength
+            };
+        }
+    }
 
     [Fact(DisplayName = nameof(NotNullOk))]
     [Trait("Domain", "DomainValidation - Validation")]
@@ -52,5 +68,15 @@ public class DomainValidationTest
         Action action = () => DomainValidation.NotNullOrEmpty(target, "fieldName");
 
         action.Should().NotThrow();
+    }
+
+    [Theory(DisplayName = nameof(NotNullOrEmptyThrowWhenEmpty))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    [MemberData(nameof(GetValuesLessThanMin), parameters: 10)]
+    public void MinLengthThrowWhenLess(string target, int minLength)
+    {
+        Action action = () => DomainValidation.MinLength(target, minLength, "fieldName");
+
+        action.Should().Throw<EntityValidationException>().WithMessage($"fieldName should not be less than {minLength}");
     }
 }
