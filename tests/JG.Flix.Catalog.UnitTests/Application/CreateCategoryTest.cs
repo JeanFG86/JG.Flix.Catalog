d33006afd77c4e020e6eeaead9843fd75ea5eb99
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-06-27 20:35:37 -0300

    create test ThrowWhenCantInstantiateAggregateAsync

--- tests/JG.Flix.Catalog.UnitTests/Application/CreateCategoryTest.cs
+++ tests/JG.Flix.Catalog.UnitTests/Application/CreateCategoryTest.cs
@@ -1,10 +1,10 @@
 ﻿using FluentAssertions;
-using JG.Flix.Catalog.Application.Interfaces;
+using JG.Flix.Catalog.Application.UseCases.Category.CreateCategory;
 using JG.Flix.Catalog.Domain.Entity;
-using JG.Flix.Catalog.Domain.Repository;
+using JG.Flix.Catalog.Domain.Exceptions;
+using JG.Flix.Catalog.UnitTests.Domain.Entity.Category;
 using Moq;
 using Xunit;
-using UseCases = JG.Flix.Catalog.Application.UseCases.Category.CreateCategory;
 
 namespace JG.Flix.Catalog.UnitTests.Application;
 
@@ -25,7 +25,7 @@ public class CreateCategoryTest
         var repositoryMock = _fixture.GetRepositoryMock();
         var unitOfWorkMock = _fixture.GetUnitOfWorkMock();
 
-        var useCase = new UseCases.CreateCategory(repositoryMock.Object, unitOfWorkMock.Object);
+        var useCase = new CreateCategory(repositoryMock.Object, unitOfWorkMock.Object);
         var input = _fixture.GetInput();
 
         var output = await useCase.Handle(input, CancellationToken.None);
@@ -47,5 +47,40 @@ public class CreateCategoryTest
         output.IsActive.Should().Be(input.IsActive);
         output.Id.Should().NotBeEmpty();
         output.CreatedAt.Should().NotBeSameDateAs(default);
+    }
+
+    [Theory(DisplayName = nameof(ThrowWhenCantInstantiateAggregateAsync))]
+    [Trait("Application", "CreateCategory Use Cases")]
+    [MemberData(nameof(GetInvalidInputs))]
+    public async Task ThrowWhenCantInstantiateAggregateAsync(CreateCategoryInput input, string exceptionMessage)
+    {
+        var useCase = new CreateCategory(_fixture.GetRepositoryMock().Object, _fixture.GetUnitOfWorkMock().Object);
+
+        Func<Task> task = async () => await useCase.Handle(input, CancellationToken.None);
+
+        await task.Should().ThrowAsync<EntityValidationException>().WithMessage(exceptionMessage);
+    }
+
+    public static IEnumerable<object[]> GetInvalidInputs() 
+    {
+        var fixture = new CreateCategoryTestFixture();
+        var invalidInputsList = new List<object[]>();
+
+        var invalidInputShortName = fixture.GetInput();
+        invalidInputShortName.Name = invalidInputShortName.Name.Substring(0, 2);
+        invalidInputsList.Add(new object[] { invalidInputShortName, "Name should be at least 3 characteres long" });
+
+        var invalidInputShortTooLongName = fixture.GetInput();
+        var tooLongNameForCategory = fixture.Faker.Commerce.ProductName();
+        while (tooLongNameForCategory.Length <= 255)
+        {
+            tooLongNameForCategory = $"{tooLongNameForCategory} {fixture.Faker.Commerce.ProductName()}";
+        }
+        invalidInputShortTooLongName.Name = tooLongNameForCategory;
+        invalidInputsList.Add(new object[] { invalidInputShortTooLongName, "Name should be less or equal 255 characteres long" });
+
+
+
+        return invalidInputsList;
     }
 }
