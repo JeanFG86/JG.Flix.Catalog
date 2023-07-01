d4cea1009885ba70ac99bb0468217a36805d8a88
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-07-01 16:27:24 -0300

    create GetCategoryInputValidatorTest and use FluentValidator

---
+++ tests/JG.Flix.Catalog.UnitTests/Application/GetCategory/GetCategoryInputValidatorTest.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using JG.Flix.Catalog.Application.UseCases.Category.GetCategory;
+using Xunit;
+
+namespace JG.Flix.Catalog.UnitTests.Application.GetCategory;
+
+[Collection(nameof(GetCategoryTestFixture))]
+public class GetCategoryInputValidatorTest
+{
+    private readonly GetCategoryTestFixture _fixture;
+
+    public GetCategoryInputValidatorTest(GetCategoryTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(ValidationOk))]
+    [Trait("Application", "GetCategoryInputValidation - UseCases")]
+    public void ValidationOk()
+    {
+        var validInput = new GetCategoryInput(Guid.NewGuid());
+        var validator = new GetCategoryInputValidator();
+
+        var validationResult = validator.Validate(validInput);
+
+        validationResult.Should().NotBeNull();
+        validationResult.IsValid.Should().BeTrue();
+        validationResult.Errors.Should().HaveCount(0);
+    }
+}
