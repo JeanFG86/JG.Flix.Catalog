76b441adfcb28e7d43acd9b6249083c154bb2815
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-07-03 18:50:36 -0300

    create UpdateCategoryTestFixture

---
+++ tests/JG.Flix.Catalog.UnitTests/Application/UpdateCategory/UpdateCategoryTestFixture.cs
@@ -0,0 +1,11 @@
+﻿using JG.Flix.Catalog.UnitTests.Common;
+using Xunit;
+
+namespace JG.Flix.Catalog.UnitTests.Application.UpdateCategory;
+
+[CollectionDefinition(nameof(UpdateCategoryTestFixture))]
+public class UpdateCategoryTestFixtureCollection : ICollectionFixture<UpdateCategoryTestFixture> { }
+
+public class UpdateCategoryTestFixture: BaseFixture
+{
+}
