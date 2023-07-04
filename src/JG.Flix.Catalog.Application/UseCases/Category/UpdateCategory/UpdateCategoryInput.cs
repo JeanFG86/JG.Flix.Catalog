ee79040d63b2b8bf8ad5ac432763aad53e58192b
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-07-04 20:38:22 -0300

    ..

--- src/JG.Flix.Catalog.Application/UseCases/Category/UpdateCategory/UpdateCategoryInput.cs
+++ src/JG.Flix.Catalog.Application/UseCases/Category/UpdateCategory/UpdateCategoryInput.cs
@@ -1,10 +1,5 @@
 ﻿using JG.Flix.Catalog.Application.UseCases.Category.Common;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace JG.Flix.Catalog.Application.UseCases.Category.UpdateCategory;
 public class UpdateCategoryInput : IRequest<CategoryModelOutput>
