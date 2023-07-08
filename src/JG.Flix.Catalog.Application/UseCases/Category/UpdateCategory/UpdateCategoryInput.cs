e7c5058731d7f8457b358d1537b847006638a039
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-07-08 15:43:28 -0300

    create test UpdateCategoryWithoutProvidingIsActive

--- src/JG.Flix.Catalog.Application/UseCases/Category/UpdateCategory/UpdateCategoryInput.cs
+++ src/JG.Flix.Catalog.Application/UseCases/Category/UpdateCategory/UpdateCategoryInput.cs
@@ -7,9 +7,9 @@ public class UpdateCategoryInput : IRequest<CategoryModelOutput>
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
-    public bool IsActive { get; set; }
+    public bool? IsActive { get; set; }
 
-    public UpdateCategoryInput(Guid id, string name, string? description = null, bool isActive = true)
+    public UpdateCategoryInput(Guid id, string name, string? description = null, bool? isActive = null)
     {
         Id = id;
         Name = name;
--- tests/JG.Flix.Catalog.UnitTests/Application/UpdateCategory/UpdateCategoryTest.cs
+++ tests/JG.Flix.Catalog.UnitTests/Application/UpdateCategory/UpdateCategoryTest.cs
@@ -5,6 +5,7 @@ using JG.Flix.Catalog.Application.UseCases.Category.Common;
 using FluentAssertions;
 using JG.Flix.Catalog.Domain.Entity;
 using JG.Flix.Catalog.Application.Exceptions;
+using JG.Flix.Catalog.Application.UseCases.Category.UpdateCategory;
 
 namespace JG.Flix.Catalog.UnitTests.Application.UpdateCategory;
 
@@ -21,7 +22,7 @@ public class UpdateCategoryTest
     [Theory(DisplayName = nameof(UpdateCategory))]
     [Trait("Application", "UpdateCategory - Use Cases")]
     [MemberData(nameof(UpdateCategoryTestDataGenerator.GetCategoriesToUpdate), parameters: 10, MemberType = typeof(UpdateCategoryTestDataGenerator))]
-    public async Task UpdateCategory(Category exampleCategory, UseCase.UpdateCategoryInput input)
+    public async Task UpdateCategory(Category exampleCategory, UpdateCategoryInput input)
     {
         var repositoryMock = _fixture.GetRepositoryMock();
         var unitOfWorkMock = _fixture.GetUnitOfWorkMock();
@@ -33,7 +34,29 @@ public class UpdateCategoryTest
         output.Should().NotBeNull();
         output.Name.Should().Be(input.Name);
         output.Description.Should().Be(input.Description);
-        output.IsActive.Should().Be(input.IsActive);
+        output.IsActive.Should().Be((bool)input.IsActive!);
+        repositoryMock.Verify(x => x.Get(exampleCategory.Id, It.IsAny<CancellationToken>()), Times.Once);
+        repositoryMock.Verify(x => x.Update(exampleCategory, It.IsAny<CancellationToken>()), Times.Once);
+        unitOfWorkMock.Verify(x => x.Commit(It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Theory(DisplayName = nameof(UpdateCategoryWithoutProvidingIsActive))]
+    [Trait("Application", "UpdateCategory - Use Cases")]
+    [MemberData(nameof(UpdateCategoryTestDataGenerator.GetCategoriesToUpdate), parameters: 10, MemberType = typeof(UpdateCategoryTestDataGenerator))]
+    public async Task UpdateCategoryWithoutProvidingIsActive(Category exampleCategory, UpdateCategoryInput exampleInput)
+    {
+        var input = new UpdateCategoryInput(exampleInput.Id, exampleInput.Name, exampleInput.Description);
+        var repositoryMock = _fixture.GetRepositoryMock();
+        var unitOfWorkMock = _fixture.GetUnitOfWorkMock();
+        repositoryMock.Setup(x => x.Get(exampleCategory.Id, It.IsAny<CancellationToken>())).ReturnsAsync(exampleCategory);
+        var usecase = new UseCase.UpdateCategory(repositoryMock.Object, unitOfWorkMock.Object);
+
+        CategoryModelOutput output = await usecase.Handle(input, CancellationToken.None);
+
+        output.Should().NotBeNull();
+        output.Name.Should().Be(input.Name);
+        output.Description.Should().Be(input.Description);
+        output.IsActive.Should().Be(exampleCategory.IsActive);
         repositoryMock.Verify(x => x.Get(exampleCategory.Id, It.IsAny<CancellationToken>()), Times.Once);
         repositoryMock.Verify(x => x.Update(exampleCategory, It.IsAny<CancellationToken>()), Times.Once);
         unitOfWorkMock.Verify(x => x.Commit(It.IsAny<CancellationToken>()), Times.Once);
