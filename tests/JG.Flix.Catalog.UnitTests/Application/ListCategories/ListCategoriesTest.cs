c2bdab3d0c18103c537acd6a73953887ae98752a
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-07-09 17:01:38 -0300

    create test List

--- tests/JG.Flix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTest.cs
+++ tests/JG.Flix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTest.cs
@@ -1,4 +1,6 @@
-﻿using Xunit;
+﻿using JG.Flix.Catalog.Domain.Entity;
+using Moq;
+using Xunit;
 
 namespace JG.Flix.Catalog.UnitTests.Application.ListCategories;
 
@@ -10,5 +12,34 @@ public class ListCategoriesTest
     public ListCategoriesTest(ListCategoriesTestFixture fixture)
     {
         _fixture = fixture;
-    }    
+    }
+
+    [Fact(DisplayName = nameof(List))]
+    [Trait("Application", "ListCAtegories - Use Cases")]
+    public async Task List()
+    {
+        var categoriesExampleList = _fixture.GetExampleCategoriesList();
+        var repositoryMock = _fixture.GetRepositoryMock();
+        var input = new ListCategoriesInput(page: 2, perPage: 15, search: "search-example", sort: "name", dir: SearchOrder.Asc);
+        repositoryMock.Setup(x => x.Search(
+            It.Is<SearchInput>(
+                searchInput.Page == input.Page
+                && searchInput.PerPage == input.PerPage
+                && searchInput.Search == input.Search
+                && searchInput.OrderBy == input.Sort
+                && searchInput.OrderBy == input.Dir
+            ),
+            It.IsAny<CancellationToken>()
+       )).ReturnsAsync(new OutputSearch<Category>(
+              currentPage: input.Page,  
+              perPage: input.PerPage,
+              itens: (IReadOnlyList<Category>)categoriesExampleList,
+              total: 70
+       ));
+        var useCases = new ListCategories(repositoryMock.Object);
+
+        var outPut = await useCases.Handle(input, CancellationToken.None);
+
+        outPut.Should().NotBeNull();
+    }
 }
--- tests/JG.Flix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTestFixture.cs
+++ tests/JG.Flix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTestFixture.cs
@@ -1,4 +1,5 @@
 ﻿using JG.Flix.Catalog.Application.Interfaces;
+using JG.Flix.Catalog.Domain.Entity;
 using JG.Flix.Catalog.Domain.Repository;
 using JG.Flix.Catalog.UnitTests.Common;
 using Moq;
@@ -13,5 +14,37 @@ public class ListCategoriesTestFixtureCollection : ICollectionFixture<ListCatego
 public class ListCategoriesTestFixture: BaseFixture
 {
     public Mock<ICategoryRepository> GetRepositoryMock() => new();
-    public Mock<IUnitOfWork> GetUnitOfWorkMock() => new();
+
+    public string GetValidCategoryName()
+    {
+        var categoryName = "";
+        while (categoryName.Length < 3)
+            categoryName = Faker.Commerce.Categories(1)[0];
+
+        if (categoryName.Length > 255)
+            categoryName = categoryName[..255];
+
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription = Faker.Commerce.ProductDescription();
+
+        if (categoryDescription.Length > 10_000)
+            categoryDescription = categoryDescription[..10_000];
+
+        return categoryDescription;
+    }
+
+    public bool GetRandonBoolean() => new Random().NextDouble() < 0.5;
+
+    public Category GetExampleCategory() => new(GetValidCategoryName(), GetValidCategoryDescription(), GetRandonBoolean());
+
+    public List<Category> GetExampleCategoriesList(int length = 10)
+    {
+        return Enumerable.Range(0, length)
+                         .Select(_ => GetExampleCategory())
+                         .ToList();
+    }
 }
