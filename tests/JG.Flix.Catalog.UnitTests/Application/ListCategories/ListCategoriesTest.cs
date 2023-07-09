4b848adca3fe5891bfed96bd9c105f3660afb281
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-07-09 16:38:29 -0300

    create ListCategoriesTest

---
+++ tests/JG.Flix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace JG.Flix.Catalog.UnitTests.Application.ListCategories;
+
+[Collection(nameof(ListCategoriesTestFixture))]
+public class ListCategoriesTest
+{
+    private readonly ListCategoriesTestFixture _fixture;
+
+    public ListCategoriesTest(ListCategoriesTestFixture fixture)
+    {
+        _fixture = fixture;
+    }    
+}
---
+++ tests/JG.Flix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTestFixture.cs
@@ -0,0 +1,17 @@
+﻿using JG.Flix.Catalog.Application.Interfaces;
+using JG.Flix.Catalog.Domain.Repository;
+using JG.Flix.Catalog.UnitTests.Common;
+using Moq;
+using Xunit;
+
+namespace JG.Flix.Catalog.UnitTests.Application.ListCategories;
+
+
+[CollectionDefinition(nameof(ListCategoriesTestFixture))]
+public class ListCategoriesTestFixtureCollection : ICollectionFixture<ListCategoriesTestFixture> { }
+
+public class ListCategoriesTestFixture: BaseFixture
+{
+    public Mock<ICategoryRepository> GetRepositoryMock() => new();
+    public Mock<IUnitOfWork> GetUnitOfWorkMock() => new();
+}
