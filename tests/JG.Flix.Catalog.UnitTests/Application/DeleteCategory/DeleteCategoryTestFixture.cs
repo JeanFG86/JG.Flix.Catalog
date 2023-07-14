ab1000a5dec67c70a02add7550f9a4ef14c02666
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-07-14 19:43:25 -0300

    use CategoryUseCasesBaseFixture on DeleteCategoryTestFixture

--- tests/JG.Flix.Catalog.UnitTests/Application/DeleteCategory/DeleteCategoryTestFixture.cs
+++ tests/JG.Flix.Catalog.UnitTests/Application/DeleteCategory/DeleteCategoryTestFixture.cs
@@ -1,6 +1,7 @@
 ﻿using JG.Flix.Catalog.Application.Interfaces;
 using JG.Flix.Catalog.Domain.Entity;
 using JG.Flix.Catalog.Domain.Repository;
+using JG.Flix.Catalog.UnitTests.Application.Common;
 using JG.Flix.Catalog.UnitTests.Common;
 using Moq;
 using Xunit;
@@ -10,32 +11,7 @@ namespace JG.Flix.Catalog.UnitTests.Application.DeleteCategory;
 [CollectionDefinition(nameof(DeleteCategoryTestFixture))]
 public class DeleteCategoryTestFixtureCollection : ICollectionFixture<DeleteCategoryTestFixture> { }
 
-public class DeleteCategoryTestFixture: BaseFixture
+public class DeleteCategoryTestFixture: CategoryUseCasesBaseFixture
 {
-    public Mock<ICategoryRepository> GetRepositoryMock() => new();
-    public Mock<IUnitOfWork> GetUnitOfWorkMock() => new();
-
-    public string GetValidCategoryName()
-    {
-        var categoryName = "";
-        while (categoryName.Length < 3)
-            categoryName = Faker.Commerce.Categories(1)[0];
-
-        if (categoryName.Length > 255)
-            categoryName = categoryName[..255];
-
-        return categoryName;
-    }
-
-    public string GetValidCategoryDescription()
-    {
-        var categoryDescription = Faker.Commerce.ProductDescription();
-
-        if (categoryDescription.Length > 10_000)
-            categoryDescription = categoryDescription[..10_000];
-
-        return categoryDescription;
-    }
-
     public Category GetValidCategory() => new(GetValidCategoryName(), GetValidCategoryDescription());
 }
