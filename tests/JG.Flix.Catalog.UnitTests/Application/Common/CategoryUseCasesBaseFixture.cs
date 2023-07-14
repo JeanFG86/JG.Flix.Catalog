8ae0534251671773a82aed9936067e86f9971d5c
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-07-14 19:42:25 -0300

    create CategoryUseCasesBaseFixture

---
+++ tests/JG.Flix.Catalog.UnitTests/Application/Common/CategoryUseCasesBaseFixture.cs
@@ -0,0 +1,36 @@
+﻿
+using JG.Flix.Catalog.Application.Interfaces;
+using JG.Flix.Catalog.Domain.Repository;
+using JG.Flix.Catalog.UnitTests.Common;
+using Moq;
+
+namespace JG.Flix.Catalog.UnitTests.Application.Common;
+public abstract class CategoryUseCasesBaseFixture : BaseFixture
+{
+    public string GetValidCategoryName()
+    {
+        var categoryName = "";
+        while (categoryName.Length < 3)
+            categoryName = Faker.Commerce.Categories(1)[0];
+
+        if (categoryName.Length > 255)
+            categoryName = categoryName[..255];
+
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription = Faker.Commerce.ProductDescription();
+
+        if (categoryDescription.Length > 10_000)
+            categoryDescription = categoryDescription[..10_000];
+
+        return categoryDescription;
+    }
+
+    public bool GetRandonBoolean() => new Random().NextDouble() < 0.5;
+
+    public Mock<ICategoryRepository> GetRepositoryMock() => new();
+    public Mock<IUnitOfWork> GetUnitOfWorkMock() => new();
+}
--- tests/JG.Flix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTestFixture.cs
+++ tests/JG.Flix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTestFixture.cs
@@ -1,6 +1,7 @@
 ﻿using JG.Flix.Catalog.Application.Interfaces;
 using JG.Flix.Catalog.Application.UseCases.Category.CreateCategory;
 using JG.Flix.Catalog.Domain.Repository;
+using JG.Flix.Catalog.UnitTests.Application.Common;
 using JG.Flix.Catalog.UnitTests.Common;
 using Moq;
 using Xunit;
@@ -12,36 +13,10 @@ public class CreateCategoryTestFixtureCollection : ICollectionFixture<CreateCate
 {
 }
 
-public class CreateCategoryTestFixture : BaseFixture
+public class CreateCategoryTestFixture : CategoryUseCasesBaseFixture
 {
-    public string GetValidCategoryName()
-    {
-        var categoryName = "";
-        while (categoryName.Length < 3)
-            categoryName = Faker.Commerce.Categories(1)[0];
-
-        if (categoryName.Length > 255)
-            categoryName = categoryName[..255];
-
-        return categoryName;
-    }
-
-    public string GetValidCategoryDescription()
-    {
-        var categoryDescription = Faker.Commerce.ProductDescription();
-
-        if (categoryDescription.Length > 10_000)
-            categoryDescription = categoryDescription[..10_000];
-
-        return categoryDescription;
-    }
-
-    public bool GetRandonBoolean() => new Random().NextDouble() < 0.5;
-
-    public CreateCategoryInput GetInput() => new(GetValidCategoryName(), GetValidCategoryDescription(), GetRandonBoolean());
-
-    public Mock<ICategoryRepository> GetRepositoryMock() => new();
-    public Mock<IUnitOfWork> GetUnitOfWorkMock() => new();
+    
+    public CreateCategoryInput GetInput() => new(GetValidCategoryName(), GetValidCategoryDescription(), GetRandonBoolean());    
 
     public CreateCategoryInput GetInvalidInputShortName()
     {
