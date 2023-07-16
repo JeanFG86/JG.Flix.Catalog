a2dbdeab44be69c8e128edee7994db931bdedca4
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-07-16 17:34:38 -0300

    create project JG.Flix.Catalog.Infra.Data.EF

---
+++ src/JG.Flix.Catalog.Infra.Data.EF/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using JG.Flix.Catalog.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace JG.Flix.Catalog.Infra.Data.EF.Configurations;
+internal class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.HasKey(category => category.Id);
+        builder.Property(category => category.Name).HasMaxLength(255);
+        builder.Property(category => category.Description).HasMaxLength(10_000);
+    }
+}
---
+++ src/JG.Flix.Catalog.Infra.Data.EF/FlixCatalogDbContext.cs
@@ -0,0 +1,18 @@
+﻿using JG.Flix.Catalog.Domain.Entity;
+using JG.Flix.Catalog.Infra.Data.EF.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace JG.Flix.Catalog.Infra.Data.EF;
+public class FlixCatalogDbContext : DbContext
+{
+    public DbSet<Category> Categories => Set<Category>();
+
+    public FlixCatalogDbContext(DbContextOptions<FlixCatalogDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+    }
+}
