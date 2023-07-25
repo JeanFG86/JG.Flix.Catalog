b0a2d4aff3e5e04266c1d32d2429f9696f86608f
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-07-25 20:56:56 -0300

    create test GetCategory

---
+++ tests/JG.Flix.Catalog.IntegrationTests/Application/UseCases/Category/GetCategory/GetCategoryTestFixture.cs
@@ -0,0 +1,12 @@
+﻿using JG.Flix.Catalog.IntegrationTests.Application.UseCases.Category.Common;
+using Xunit;
+
+namespace JG.Flix.Catalog.IntegrationTests.Application.UseCases.Category.GetCategory;
+
+
+[CollectionDefinition(nameof(GetCategoryTestFixture))]
+public class GetCategoryTestFixtureCollecton : ICollectionFixture<GetCategoryTestFixture> { }
+
+public class GetCategoryTestFixture : CategoryUseCasesBaseFixture
+{
+}
