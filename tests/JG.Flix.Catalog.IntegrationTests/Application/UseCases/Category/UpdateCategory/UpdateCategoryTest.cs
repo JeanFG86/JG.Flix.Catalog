3be2c43fc5df7cc77d6cd4a037974a74ad7f8f9c
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-07-29 14:37:51 -0300

    create integration test UpdateCategory

---
+++ tests/JG.Flix.Catalog.IntegrationTests/Application/UseCases/Category/UpdateCategory/UpdateCategoryTest.cs
@@ -0,0 +1,50 @@
+﻿using JG.Flix.Catalog.Application.UseCases.Category.Common;
+using JG.Flix.Catalog.Application.UseCases.Category.UpdateCategory;
+using DomainEntity = JG.Flix.Catalog.Domain.Entity;
+using ApplicationUseCase = JG.Flix.Catalog.Application.UseCases.Category.UpdateCategory;
+using JG.Flix.Catalog.Infra.Data.EF.Repositories;
+using JG.Flix.Catalog.Infra.Data.EF;
+using FluentAssertions;
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+
+namespace JG.Flix.Catalog.IntegrationTests.Application.UseCases.Category.UpdateCategory;
+
+[Collection(nameof(UpdateCategoryTestFixture))]
+public class UpdateCategoryTest
+{
+    private readonly UpdateCategoryTestFixture _fixture;
+
+    public UpdateCategoryTest(UpdateCategoryTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Theory(DisplayName = nameof(UpdateCategory))]
+    [Trait("Integration/Application", "UpdateCategory - Use Cases")]
+    [MemberData(nameof(UpdateCategoryTestDataGenerator.GetCategoriesToUpdate), parameters: 5, MemberType = typeof(UpdateCategoryTestDataGenerator))]
+    public async Task UpdateCategory(DomainEntity.Category exampleCategory, UpdateCategoryInput input)
+    {
+        var dbContext = _fixture.CreateDbContext();
+        await dbContext.AddRangeAsync(_fixture.GetExampleCategoryList());
+        var trackingInfo = await dbContext.AddAsync(exampleCategory);
+        dbContext.SaveChanges();
+        trackingInfo.State = EntityState.Detached;
+        var repository = new CategoryRepository(dbContext);
+        var unitOfWork = new UnitOfWork(dbContext);
+        var useCase = new ApplicationUseCase.UpdateCategory(repository, unitOfWork);
+
+        CategoryModelOutput output = await useCase.Handle(input, CancellationToken.None);
+
+        var dbCategory = await (_fixture.CreateDbContext(true)).Categories.FindAsync(output.Id);
+        dbCategory.Should().NotBeNull();
+        dbCategory!.Name.Should().Be(input.Name);
+        dbCategory.Description.Should().Be(input.Description);
+        dbCategory.IsActive.Should().Be((bool)input.IsActive!);
+        output.Should().NotBeNull();
+        output.Name.Should().Be(input.Name);
+        output.Description.Should().Be(input.Description);
+        output.IsActive.Should().Be((bool)input.IsActive!);
+        
+    }
+}
---
+++ tests/JG.Flix.Catalog.IntegrationTests/Application/UseCases/Category/UpdateCategory/UpdateCategoryTestDataGenerator.cs
@@ -0,0 +1,45 @@
+﻿namespace JG.Flix.Catalog.IntegrationTests.Application.UseCases.Category.UpdateCategory;
+public class UpdateCategoryTestDataGenerator
+{
+    public static IEnumerable<object[]> GetCategoriesToUpdate(int times = 10)
+    {
+        var fixture = new UpdateCategoryTestFixture();
+        for (int indice = 0; indice < times; indice++)
+        {
+            var exampleCategory = fixture.GetExampleCategory();
+            var exampleInput = fixture.GetValidInput(exampleCategory.Id);
+
+            yield return new object[]
+            {
+                exampleCategory, exampleInput
+            };
+
+        }
+    }
+
+    public static IEnumerable<object[]> GetInvalidInputs(int times = 12)
+    {
+        var fixture = new UpdateCategoryTestFixture();
+        var invalidInputsList = new List<object[]>();
+        var totalInvalidCases = 3;
+
+        for (int index = 0; index < times; index++)
+        {
+            switch (index % totalInvalidCases)
+            {
+                case 0:
+                    invalidInputsList.Add(new object[] { fixture.GetInvalidInputShortName(), "Name should be at least 3 characteres long" });
+                    break;
+                case 1:
+                    invalidInputsList.Add(new object[] { fixture.GetInvalidInputTooLongName(), "Name should be less or equal 255 characteres long" });
+                    break;
+                case 2:
+                    invalidInputsList.Add(new object[] { fixture.GetInvalidCategoryTooLongDescription(), "Description should be less or equal 10000 characteres long" });
+                    break;
+                default:
+                    break;
+            }
+        }
+        return invalidInputsList;
+    }
+}
