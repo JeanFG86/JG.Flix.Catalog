d6f9d031c45e31941c84da5aa5961c9848c9a4de
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-07-31 21:57:56 -0300

    create SearhReturnsListAndTotal test

---
+++ tests/JG.Flix.Catalog.IntegrationTests/Application/UseCases/Category/ListCategories/ListCategoriesTest.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using JG.Flix.Catalog.Infra.Data.EF;
+using Xunit;
+using JG.Flix.Catalog.Infra.Data.EF.Repositories;
+using AppUseCases = JG.Flix.Catalog.Application.UseCases.Category.ListCategories;
+using JG.Flix.Catalog.Application.UseCases.Category.Common;
+
+namespace JG.Flix.Catalog.IntegrationTests.Application.UseCases.Category.ListCategories;
+[Collection(nameof(ListCategoriesTestFixture))]
+public class ListCategoriesTest
+{
+    private readonly ListCategoriesTestFixture _fixture;
+
+    public ListCategoriesTest(ListCategoriesTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(SearhReturnsListAndTotal))]
+    [Trait("Integration/Application", "ListCategories - Use Cases")]
+    public async Task SearhReturnsListAndTotal()
+    {
+        FlixCatalogDbContext dbContext = _fixture.CreateDbContext();
+        var exampleCategoryList = _fixture.GetExampleCategoryList(10);
+        await dbContext.AddRangeAsync(exampleCategoryList);
+        await dbContext.SaveChangesAsync();
+        var categoryRepository = new CategoryRepository(dbContext);
+        var input = new AppUseCases.ListCategoriesInput(1, 20);
+        var useCase = new AppUseCases.ListCategories(categoryRepository);
+
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        output.Should().NotBeNull();
+        output.Items.Should().NotBeNull();
+        output.Page.Should().Be(input.Page);
+        output.PerPage.Should().Be(input.PerPage);
+        output.Total.Should().Be(exampleCategoryList.Count);
+        output.Items.Should().HaveCount(exampleCategoryList.Count);
+
+        foreach (CategoryModelOutput outputItem in output.Items)
+        {
+            var exampleItem = exampleCategoryList.Find(category => category.Id == outputItem.Id);
+            exampleItem.Should().NotBeNull();
+            outputItem!.Name.Should().Be(exampleItem!.Name);
+            outputItem.Description.Should().Be(exampleItem.Description);
+            outputItem.IsActive.Should().Be(exampleItem.IsActive);
+        }
+    }
+}
---
+++ tests/JG.Flix.Catalog.IntegrationTests/Application/UseCases/Category/ListCategories/ListCategoriesTestFixture.cs
@@ -0,0 +1,12 @@
+﻿
+using JG.Flix.Catalog.IntegrationTests.Application.UseCases.Category.Common;
+using Xunit;
+
+namespace JG.Flix.Catalog.IntegrationTests.Application.UseCases.Category.ListCategories;
+
+[CollectionDefinition(nameof(ListCategoriesTestFixture))]
+public class ListCategoriesTestFixtureCollection : ICollectionFixture<ListCategoriesTestFixture> { }
+
+public class ListCategoriesTestFixture : CategoryUseCasesBaseFixture
+{
+}
