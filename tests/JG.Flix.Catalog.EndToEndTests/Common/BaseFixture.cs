7dcf0dbd1b8ff392084dd0b2f5bea25f22c01ec2
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-08-03 22:28:50 -0300

    create e2e project test and baseFixture

---
+++ tests/JG.Flix.Catalog.EndToEndTests/Common/BaseFixture.cs
@@ -0,0 +1,18 @@
+﻿using Bogus;
+using JG.Flix.Catalog.Infra.Data.EF;
+using Microsoft.EntityFrameworkCore;
+
+namespace JG.Flix.Catalog.EndToEndTests.Common;
+public abstract class BaseFixture
+{
+    public Faker Faker { get; set; }
+
+    protected BaseFixture() => Faker = new Faker("pt_BR");
+
+    public FlixCatalogDbContext CreateDbContext()
+    {
+        var context = new FlixCatalogDbContext(new DbContextOptionsBuilder<FlixCatalogDbContext>().UseInMemoryDatabase("end2end-test-db").Options);
+
+        return context;
+    }
+}
