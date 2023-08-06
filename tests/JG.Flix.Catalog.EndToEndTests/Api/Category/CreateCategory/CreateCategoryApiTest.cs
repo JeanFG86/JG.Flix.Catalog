d2a0acb3040cb879a352c67708f30451c97f2884
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-08-06 16:33:37 -0300

    create CreateCategory test

--- tests/JG.Flix.Catalog.EndToEndTests/Api/Category/CreateCategory/CreateCategoryApiTest.cs
+++ tests/JG.Flix.Catalog.EndToEndTests/Api/Category/CreateCategory/CreateCategoryApiTest.cs
@@ -1,4 +1,31 @@
-﻿namespace JG.Flix.Catalog.EndToEndTests.Api.Category.CreateCategory;
+﻿using FluentAssertions;
+using JG.Flix.Catalog.Application.UseCases.Category.Common;
+
+namespace JG.Flix.Catalog.EndToEndTests.Api.Category.CreateCategory;
+
+[Collection(nameof(CreateCategoryApiTestFixture))]
 public class CreateCategoryApiTest
 {
+    private readonly CreateCategoryApiTestFixture _fixture;
+
+    public CreateCategoryApiTest(CreateCategoryApiTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = "")]
+    [Trait("EndToEnd/API", "Category - Endpoints")]
+    public async Task CreateCategory()
+    {
+        var input =  _fixture.getExampleInput();
+
+        CategoryModelOutput output = await _fixture.Api.Post<CategoryModelOutput>("/categories", input);
+
+        output.Should().NotBeNull();
+        output.Name.Should().Be(input.Name);
+        output.Description.Should().Be(input.Description);
+        output.IsActive.Should().Be(input.IsActive);
+        output.Id.Should().NotBeEmpty();
+        output.CreatedAt.Should().NotBeSameDateAs(default);
+    }
 }
--- tests/JG.Flix.Catalog.EndToEndTests/Api/Category/CreateCategory/CreateCategoryApiTestFixture.cs
+++ tests/JG.Flix.Catalog.EndToEndTests/Api/Category/CreateCategory/CreateCategoryApiTestFixture.cs
@@ -1,6 +1,14 @@
-﻿using JG.Flix.Catalog.EndToEndTests.Api.Category.Common;
+﻿using JG.Flix.Catalog.Application.UseCases.Category.CreateCategory;
+using JG.Flix.Catalog.EndToEndTests.Api.Category.Common;
 
 namespace JG.Flix.Catalog.EndToEndTests.Api.Category.CreateCategory;
+
+[CollectionDefinition(nameof(CreateCategoryApiTestFixture))]
+public class CreateCategoryApiTestFixtureCollection: ICollectionFixture<CreateCategoryApiTestFixture>
+{}
+
+
 public class CreateCategoryApiTestFixture : CategoryBaseFixture
 {
+    public CreateCategoryInput getExampleInput() => new(GetValidCategoryName(), GetValidCategoryDescription(), GetRandonBoolean());
 }
