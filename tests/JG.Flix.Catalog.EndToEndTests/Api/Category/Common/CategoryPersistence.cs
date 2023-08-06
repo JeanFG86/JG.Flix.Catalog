f7ba8f0035d285eb2b97565e3e860e974a8e4552
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-08-06 17:38:45 -0300

    create CategoryPersistence

---
+++ tests/JG.Flix.Catalog.EndToEndTests/Api/Category/Common/CategoryPersistence.cs
@@ -0,0 +1,19 @@
+﻿using JG.Flix.Catalog.Infra.Data.EF;
+using Microsoft.EntityFrameworkCore;
+using DomainEntity = JG.Flix.Catalog.Domain.Entity;
+
+namespace JG.Flix.Catalog.EndToEndTests.Api.Category.Common;
+public class CategoryPersistence
+{
+    private readonly FlixCatalogDbContext _context;
+
+    public CategoryPersistence(FlixCatalogDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<DomainEntity.Category?> GetById(Guid id)
+    {
+        return await _context.Categories.AsNoTracking().FirstOrDefaultAsync(c => c.Id == id);
+    }
+}
