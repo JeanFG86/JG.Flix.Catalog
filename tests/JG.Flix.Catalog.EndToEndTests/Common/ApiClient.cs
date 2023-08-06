13ea1dfa60eb1eddfa525fcdbb2e9156f6e1c11f
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-08-06 17:22:09 -0300

    implementes Post methos in ApiClient

--- tests/JG.Flix.Catalog.EndToEndTests/Common/ApiClient.cs
+++ tests/JG.Flix.Catalog.EndToEndTests/Common/ApiClient.cs
@@ -1,4 +1,7 @@
-﻿namespace JG.Flix.Catalog.EndToEndTests.Common;
+﻿using System.Text;
+using System.Text.Json;
+
+namespace JG.Flix.Catalog.EndToEndTests.Common;
 public class ApiClient
 {
     private readonly HttpClient _httpClient;
@@ -6,5 +9,17 @@ public class ApiClient
     public ApiClient(HttpClient httpClient)
     {
         _httpClient = httpClient;
+    }
+
+    public async Task<(HttpResponseMessage?, TOutput?)> Post<TOutput>(string route, object payload)
+    {
+        var response = await _httpClient.PostAsync(route, new StringContent(JsonSerializer.Serialize(payload), Encoding.UTF8, "application/json"));
+        var outputString = await response.Content.ReadAsStringAsync();
+        var output = JsonSerializer.Deserialize<TOutput>(outputString,
+            new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+        return (response, output);
     }
 }
