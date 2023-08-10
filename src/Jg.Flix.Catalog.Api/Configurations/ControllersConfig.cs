6e424e04aaa9f9fbf6f8082286621f0864fd6ffa
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-08-09 21:47:42 -0300

    create ControllersConfig

---
+++ src/Jg.Flix.Catalog.Api/Configurations/ControllersConfig.cs
@@ -0,0 +1,31 @@
+﻿namespace Jg.Flix.Catalog.Api.Configurations;
+
+public static class ControllersConfig
+{
+    public static IServiceCollection AddAndConfigureControllers(this IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddDocumentation();
+
+        return services;
+    }
+
+    private static IServiceCollection AddDocumentation(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        return services;
+    }
+
+    public static WebApplication UseDocumentation(this WebApplication app)
+    {
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        return app;
+    }
+}
--- src/Jg.Flix.Catalog.Api/Program.cs
+++ src/Jg.Flix.Catalog.Api/Program.cs
@@ -1,20 +1,13 @@
+using Jg.Flix.Catalog.Api.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
+builder.Services
+    .AddUseCases()
+    .AddAndConfigureControllers();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseDocumentation();
 
 app.UseHttpsRedirection();
 
