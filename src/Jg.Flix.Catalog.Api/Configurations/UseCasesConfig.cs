e8d81f3a93407f1935b0e81044ef448d24de0836
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-08-09 21:19:14 -0300

    create UseCasesConfig and update libraries

---
+++ src/Jg.Flix.Catalog.Api/Configurations/UseCasesConfig.cs
@@ -0,0 +1,15 @@
+﻿using JG.Flix.Catalog.Application.UseCases.Category.CreateCategory;
+
+namespace Jg.Flix.Catalog.Api.Configurations
+{
+    public static class UseCasesConfig
+    {
+        public static IServiceCollection AddUseCases(
+        this IServiceCollection services
+    )
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(CreateCategory).Assembly));
+            return services;
+        }
+    }
+}
