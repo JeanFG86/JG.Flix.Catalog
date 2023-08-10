1263f490072ff44a30620c2256b179d2efb24700
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-08-09 21:57:44 -0300

    create AddRepositories

--- src/Jg.Flix.Catalog.Api/Configurations/UseCasesConfig.cs
+++ src/Jg.Flix.Catalog.Api/Configurations/UseCasesConfig.cs
@@ -1,15 +1,26 @@
-﻿using JG.Flix.Catalog.Application.UseCases.Category.CreateCategory;
+﻿using JG.Flix.Catalog.Application.Interfaces;
+using JG.Flix.Catalog.Application.UseCases.Category.CreateCategory;
+using JG.Flix.Catalog.Domain.Repository;
+using JG.Flix.Catalog.Infra.Data.EF;
+using JG.Flix.Catalog.Infra.Data.EF.Repositories;
 
 namespace Jg.Flix.Catalog.Api.Configurations
 {
     public static class UseCasesConfig
     {
-        public static IServiceCollection AddUseCases(
-        this IServiceCollection services
-    )
+        public static IServiceCollection AddUseCases(this IServiceCollection services)
         {
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(CreateCategory).Assembly));
+            services.AddRepositories();
             return services;
         }
+
+        private static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddTransient<ICategoryRepository, CategoryRepository>();
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+            return services;
+        }
+
     }
 }
