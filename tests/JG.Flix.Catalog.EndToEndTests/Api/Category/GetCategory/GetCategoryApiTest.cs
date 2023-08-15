bd7e23ab0f07db18690dcd6ad669169747e29fca
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-08-15 19:03:35 -0300

    create GetCategoryApiTest

---
+++ tests/JG.Flix.Catalog.EndToEndTests/Api/Category/GetCategory/GetCategoryApiTest.cs
@@ -0,0 +1,12 @@
+﻿namespace JG.Flix.Catalog.EndToEndTests.Api.Category.GetCategory;
+
+[Collection(nameof(GetCategoryApiTestFixture))]
+public class GetCategoryApiTest
+{
+    private readonly GetCategoryApiTestFixture _fixture;
+
+    public GetCategoryApiTest(GetCategoryApiTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+}
