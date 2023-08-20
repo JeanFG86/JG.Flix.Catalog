77ee77d25254735a615f59454c5628af59ae94e2
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-08-20 18:13:42 -0300

    create DeleteCategoryApiTest

---
+++ tests/JG.Flix.Catalog.EndToEndTests/Api/Category/DeleteCategory/DeleteCategoryApiTest.cs
@@ -0,0 +1,12 @@
+﻿namespace JG.Flix.Catalog.EndToEndTests.Api.Category.DeleteCategory;
+
+[Collection(nameof(DeleteCategoryApiTestFixture))]
+public class DeleteCategoryApiTest
+{
+    private readonly DeleteCategoryApiTestFixture _fixture;
+
+    public DeleteCategoryApiTest(DeleteCategoryApiTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+}
---
+++ tests/JG.Flix.Catalog.EndToEndTests/Api/Category/DeleteCategory/DeleteCategoryApiTestFixture.cs
@@ -0,0 +1,9 @@
+﻿
+namespace JG.Flix.Catalog.EndToEndTests.Api.Category.DeleteCategory;
+
+[CollectionDefinition(nameof(DeleteCategoryApiTestFixture))]
+public class DeleteCategoryApiTestFixtureCollection : ICollectionFixture<DeleteCategoryApiTestFixture>
+{ }
+public class DeleteCategoryApiTestFixture
+{
+}
