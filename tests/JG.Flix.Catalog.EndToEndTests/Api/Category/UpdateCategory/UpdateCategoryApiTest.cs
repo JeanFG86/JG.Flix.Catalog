cafd730b4592e8490042c10d0a8590d33b1525af
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-08-26 11:03:10 -0300

    create UpdateCategoryOnlyNameAndDescription test

--- tests/JG.Flix.Catalog.EndToEndTests/Api/Category/UpdateCategory/UpdateCategoryApiTest.cs
+++ tests/JG.Flix.Catalog.EndToEndTests/Api/Category/UpdateCategory/UpdateCategoryApiTest.cs
@@ -65,4 +65,29 @@ public class UpdateCategoryApiTest
         dbCategory.Description.Should().Be(exampleCategory.Description);
         dbCategory.IsActive.Should().Be((bool)exampleCategory.IsActive);
     }
+
+    [Fact(DisplayName = nameof(UpdateCategoryOnlyNameAndDescription))]
+    [Trait("EndToEnd/API", "Category/Update - Endpoints")]
+    public async void UpdateCategoryOnlyNameAndDescription()
+    {
+        var exampleCategoriesList = _fixture.GetExampleCategoriesList(20);
+        await _fixture.Persistence.InsertList(exampleCategoriesList);
+        var exampleCategory = exampleCategoriesList[10];
+        var input = new UpdateCategoryInput(exampleCategory.Id, _fixture.GetValidCategoryName(), _fixture.GetValidCategoryDescription());
+
+        var (response, output) = await _fixture.ApiClient.Put<CategoryModelOutput>($"/categories/{exampleCategory.Id}", input);
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be((HttpStatusCode)StatusCodes.Status200OK);
+        output.Should().NotBeNull();
+        output!.Name.Should().Be(input.Name);
+        output.Description.Should().Be(input.Description);
+        output.IsActive.Should().Be((bool)exampleCategory.IsActive!);
+        output.Id.Should().Be(exampleCategory.Id);
+        var dbCategory = await _fixture.Persistence.GetById(exampleCategory.Id);
+        dbCategory.Should().NotBeNull();
+        dbCategory!.Name.Should().Be(input.Name);
+        dbCategory.Description.Should().Be(input.Description);
+        dbCategory.IsActive.Should().Be((bool)exampleCategory.IsActive);
+    }
 }
