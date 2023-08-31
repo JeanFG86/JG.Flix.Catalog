dcaef8206e6bc59e3d048270cee5beb5f27696be
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-08-30 21:28:00 -0300

    create ListCategoriesAndTotalByDefault test

--- src/Jg.Flix.Catalog.Api/Controllers/CategoriesController.cs
+++ src/Jg.Flix.Catalog.Api/Controllers/CategoriesController.cs
@@ -49,6 +49,7 @@ public class CategoriesController : ControllerBase
     [HttpPut("{id:guid}")]
     [ProducesResponseType(typeof(CategoryModelOutput), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status422UnprocessableEntity)]
     public async Task<IActionResult> Update([FromBody] UpdateCategoryInput input, CancellationToken cancellationToken)
     {
         var output = await _mediator.Send(input, cancellationToken);
---
+++ tests/JG.Flix.Catalog.EndToEndTests/Api/Category/ListCategories/ListCategoriesApiTestFixture.cs
@@ -0,0 +1,10 @@
+﻿using JG.Flix.Catalog.EndToEndTests.Api.Category.Common;
+
+namespace JG.Flix.Catalog.EndToEndTests.Api.Category.ListCategories;
+
+[CollectionDefinition(nameof(ListCategoriesApiTestFixture))]
+public class ListCategoriesApiTestFixtureCollection : ICollectionFixture<ListCategoriesApiTestFixture>
+{ }
+public class ListCategoriesApiTestFixture : CategoryBaseFixture
+{
+}
