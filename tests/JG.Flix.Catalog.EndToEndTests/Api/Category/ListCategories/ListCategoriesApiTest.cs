99bfe6819b10189a20afa964cae46e431c09ad43
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-09-02 16:18:24 -0300

    create ListPaginated test

--- tests/JG.Flix.Catalog.EndToEndTests/Api/Category/ListCategories/ListCategoriesApiTest.cs
+++ tests/JG.Flix.Catalog.EndToEndTests/Api/Category/ListCategories/ListCategoriesApiTest.cs
@@ -18,7 +18,7 @@ public class ListCategoriesApiTest : IDisposable
 
     [Fact(DisplayName = nameof(ListCategoriesAndTotalByDefault))]
     [Trait("EndToEnd/API", "Category/List - Endpoints")]
-    public async void ListCategoriesAndTotalByDefault()
+    public async Task ListCategoriesAndTotalByDefault()
     {
         var defaultPerPage = 15;
         var exampleCategoriesList = _fixture.GetExampleCategoriesList(20);
@@ -45,7 +45,7 @@ public class ListCategoriesApiTest : IDisposable
 
     [Fact(DisplayName = nameof(ItemsEmptyWhenPersistenceEmpty))]
     [Trait("EndToEnd/API", "Category/List - Endpoints")]
-    public async void ItemsEmptyWhenPersistenceEmpty()
+    public async Task ItemsEmptyWhenPersistenceEmpty()
     {
         var (response, output) = await _fixture.ApiClient.Get<ListCategoriesOutput>($"/categories");
 
@@ -58,7 +58,7 @@ public class ListCategoriesApiTest : IDisposable
 
     [Fact(DisplayName = nameof(ListCategoriesAndTotal))]
     [Trait("EndToEnd/API", "Category/List - Endpoints")]
-    public async void ListCategoriesAndTotal()
+    public async Task ListCategoriesAndTotal()
     {
         var exampleCategoriesList = _fixture.GetExampleCategoriesList(20);
         await _fixture.Persistence.InsertList(exampleCategoriesList);
@@ -73,6 +73,37 @@ public class ListCategoriesApiTest : IDisposable
         output.Page.Should().Be(input.Page);
         output.PerPage.Should().Be(input.PerPage);
         output!.Items.Should().HaveCount(input.PerPage);
+        foreach (CategoryModelOutput outputItem in output.Items)
+        {
+            var exampleItem = exampleCategoriesList.FirstOrDefault(x => x.Id == outputItem.Id);
+            exampleItem.Should().NotBeNull();
+            outputItem.Name.Should().Be(exampleItem!.Name);
+            outputItem.Description.Should().Be(exampleItem.Description);
+            outputItem.IsActive.Should().Be(exampleItem.IsActive);
+        }
+    }
+
+    [Theory(DisplayName = nameof(ListPaginated))]
+    [Trait("EndToEnd/API", "Category/List - Endpoints")]
+    [InlineData(10, 1, 5, 5)]
+    [InlineData(10, 2, 5, 5)]
+    [InlineData(7, 2, 5, 2)]
+    [InlineData(7, 3, 5, 0)]
+    public async Task ListPaginated(int quantityCategoryToGenerate, int page, int perPage, int expectedQuantityItems)
+    {
+        var exampleCategoriesList = _fixture.GetExampleCategoriesList(quantityCategoryToGenerate);
+        await _fixture.Persistence.InsertList(exampleCategoriesList);
+        var input = new ListCategoriesInput(page, perPage);
+
+        var (response, output) = await _fixture.ApiClient.Get<ListCategoriesOutput>($"/categories", input);
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be((HttpStatusCode)StatusCodes.Status200OK);
+        output.Should().NotBeNull();
+        output!.Total.Should().Be(exampleCategoriesList.Count);
+        output.Page.Should().Be(input.Page);
+        output.PerPage.Should().Be(input.PerPage);
+        output!.Items.Should().HaveCount(expectedQuantityItems);
         foreach (CategoryModelOutput outputItem in output.Items)
         {
             var exampleItem = exampleCategoriesList.FirstOrDefault(x => x.Id == outputItem.Id);
