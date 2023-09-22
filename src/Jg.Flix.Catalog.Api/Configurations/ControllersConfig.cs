9b905455a7f636c3a03f52d56a8c9c6a228e26c1
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-09-21 21:09:52 -0300

    create JsonSnakeCasePolicy

--- src/Jg.Flix.Catalog.Api/Configurations/ControllersConfig.cs
+++ src/Jg.Flix.Catalog.Api/Configurations/ControllersConfig.cs
@@ -1,4 +1,5 @@
-﻿using Jg.Flix.Catalog.Api.Filters;
+﻿using Jg.Flix.Catalog.Api.Configurations.Policies;
+using Jg.Flix.Catalog.Api.Filters;
 
 namespace Jg.Flix.Catalog.Api.Configurations;
 
@@ -6,7 +7,12 @@ public static class ControllersConfig
 {
     public static IServiceCollection AddAndConfigureControllers(this IServiceCollection services)
     {
-        services.AddControllers(options => options.Filters.Add(typeof(ApiGlobalExceptionFilter)));
+        services.AddControllers(options => 
+            options.Filters.Add(typeof(ApiGlobalExceptionFilter))
+            ).AddJsonOptions(jsonOpt =>
+            {
+                jsonOpt.JsonSerializerOptions.PropertyNamingPolicy = new JsonSnakeCasePolicy();
+            });
         services.AddDocumentation();
 
         return services;
---
+++ src/Jg.Flix.Catalog.Api/Configurations/Policies/JsonSnakeCasePolicy.cs
@@ -0,0 +1,9 @@
+﻿using Jg.Flix.Catalog.Api.Extensions.String;
+using System.Text.Json;
+
+namespace Jg.Flix.Catalog.Api.Configurations.Policies;
+
+class JsonSnakeCasePolicy : JsonNamingPolicy
+{
+    public override string ConvertName(string name) => name.ToSnakeCase();
+}
