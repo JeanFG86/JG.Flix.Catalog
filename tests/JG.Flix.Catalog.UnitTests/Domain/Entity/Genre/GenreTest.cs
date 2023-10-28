11101178123d11aea3e1cecfa19a7014b75a553f
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-10-27 21:34:11 -0300

    create test Instantiate Genre

---
+++ tests/JG.Flix.Catalog.UnitTests/Domain/Entity/Genre/GenreTest.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using FluentAssertions;
+using DomainEntity = JG.Flix.Catalog.Domain.Entity;
+
+namespace JG.Flix.Catalog.UnitTests.Domain.Entity.Genre;
+
+[Collection(nameof(GenreTestFixture))]
+public class GenreTest
+{
+    [Fact(DisplayName =nameof(Instantiate))]
+    [Trait("Domain", "Genre - Aggregates")]
+    public void Instantiate()
+    {
+        var genreName = "Horror";
+        var datetimeBefore = DateTime.Now;
+        var datetimeAfter = DateTime.Now.AddSeconds(1);
+
+        var genre = new DomainEntity.Genre(genreName);
+
+        genre.Should().NotBeNull();
+        genre.Name.Should().Be(genreName);
+        genre.Id.Should().NotBeEmpty();
+        genre.CreatedAt.Should().NotBeSameDateAs(default(DateTime));
+        (genre.CreatedAt >= datetimeBefore).Should().BeTrue();
+        (genre.CreatedAt <= datetimeAfter).Should().BeTrue();
+        genre.IsActive.Should().BeTrue();
+
+    }
+
+}
---
+++ tests/JG.Flix.Catalog.UnitTests/Domain/Entity/Genre/GenreTestFixture.cs
@@ -0,0 +1,11 @@
+﻿using JG.Flix.Catalog.UnitTests.Common;
+using Xunit;
+
+namespace JG.Flix.Catalog.UnitTests.Domain.Entity.Genre;
+
+[CollectionDefinition(nameof(GenreTestFixture))]
+public class GenreTestFixtureCollection : ICollectionFixture<GenreTestFixture> { }
+
+public class GenreTestFixture : BaseFixture
+{
+}
