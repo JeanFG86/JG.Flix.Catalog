29a3c91f1692236d2252bc615a9e832acf26384b
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-10-27 21:36:46 -0300

    create Entity Genre

---
+++ src/JG.Flix.Catalog.Domain/Entity/Genre.cs
@@ -0,0 +1,17 @@
+﻿
+using JG.Flix.Catalog.Domain.SeedWork;
+
+namespace JG.Flix.Catalog.Domain.Entity;
+public class Genre: AggregateRoot
+{    
+    public string Name { get; private set; }
+    public bool IsActive { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+
+    public Genre(string name)
+    {
+        Name = name;
+        IsActive = true;
+        CreatedAt = DateTime.Now;
+    }
+}
