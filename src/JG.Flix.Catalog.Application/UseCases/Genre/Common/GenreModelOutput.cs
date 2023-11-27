eea4388c008cb8134f6269c855eea8c07c76a495
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-11-27 20:06:15 -0300

    create GenreModelOutput

---
+++ src/JG.Flix.Catalog.Application/UseCases/Genre/Common/GenreModelOutput.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JG.Flix.Catalog.Application.UseCases.Genre.Common;
+public class GenreModelOutput
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public bool IsActive { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public List<Guid> Categories { get; set; }
+
+    public GenreModelOutput(Guid id, string name, bool isActive, DateTime createdAt, List<Guid> categories)
+    {
+        Id = id;
+        Name = name;
+        IsActive = isActive;
+        CreatedAt = createdAt;
+        Categories = categories;
+    }
+}
