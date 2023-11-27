c86f51bb20f19f6b7f8d50d1bc889a1d7fff910e
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-11-27 20:08:20 -0300

    create CreateGenreInput

---
+++ src/JG.Flix.Catalog.Application/UseCases/Genre/CreateGenre/CreateGenreInput.cs
@@ -0,0 +1,15 @@
+﻿using JG.Flix.Catalog.Application.UseCases.Genre.Common;
+using MediatR;
+
+namespace JG.Flix.Catalog.Application.UseCases.Genre.CreateGenre;
+public class CreateGenreInput : IRequest<GenreModelOutput>
+{
+    public string Name { get; set; }
+    public bool IsActive { get; set; }
+
+    public CreateGenreInput(string name, bool isActive)
+    {
+        Name = name;
+        IsActive = isActive;
+    }
+}
