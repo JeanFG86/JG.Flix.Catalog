48b1f8fe6f49930df32b07fe4076550bdf90c196
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-11-27 20:09:39 -0300

    create ICreateGenre

---
+++ src/JG.Flix.Catalog.Application/UseCases/Genre/CreateGenre/ICreateGenre.cs
@@ -0,0 +1,7 @@
+﻿using JG.Flix.Catalog.Application.UseCases.Genre.Common;
+using MediatR;
+
+namespace JG.Flix.Catalog.Application.UseCases.Genre.CreateGenre;
+public interface ICreateGenre: IRequestHandler<CreateGenreInput, GenreModelOutput>
+{
+}
