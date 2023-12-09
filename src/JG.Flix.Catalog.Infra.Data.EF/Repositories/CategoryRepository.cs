c6770639e760036692b819dbc6eb39eec7f51804
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2023-12-09 20:05:19 -0300

    implements interface CategoryRepository

--- src/JG.Flix.Catalog.Infra.Data.EF/Repositories/CategoryRepository.cs
+++ src/JG.Flix.Catalog.Infra.Data.EF/Repositories/CategoryRepository.cs
@@ -68,4 +68,9 @@ public class CategoryRepository : ICategoryRepository
 
         return orderedQuery;
     }
+
+    public Task<IReadOnlyList<Guid>> GetIdsListByIds(List<Guid> ids, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
